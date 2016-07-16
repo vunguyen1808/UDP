f8862cfb2f96692e142cdb0729bbe6a8b1bf9cff
vu <vupro08dt@gmail.com>
2016-07-16 11:55:25 -0400

    commit first 2 main files

--- client/client_v.c
+++ client/client_v.c
@@ -1,9 +1,28 @@
-#include <isostream>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
+#define SERVER_PORT 7747
 
+int sock;
+char message [] = "congacon vit";
+struct sockaddr_in server;
 int main()
 {
 
+//socket
+	sock = socket (AF_INET, SOCK_DGRAM, 0);
 
-	return 0;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(SERVER_PORT);
+//	server.sin_addr = inet_addr("127.0.0.1");
+	inet_aton("127.0.0.1" , &server.sin_addr);
+
+//sendto
+	sendto(sock, message, sizeof(message), 0, (const struct sockaddr*)&server, sizeof(struct sockaddr_in));
+
+  return 0;
 }
--- server/sever_v.c
+++ server/sever_v.c
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+
+#define SERVER_PORT 7747
+
+int sock, length;
+struct sockaddr_in server;
+socklen_t address_length;
+char* message = (char*) malloc (sizeof(char)*200);//[200];
 
 int main()
 {
+// socket
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock < 0)
+			perror("Opening socket");
 
-  return 0;
+	length = sizeof(server);
+//	bzero(&server,length);
+
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port=htons(SERVER_PORT);
+
+
+// bind
+	if(bind(sock, (struct sockaddr *)&server, length)<0)
+			perror("binding");
+
+	address_length = sizeof(struct sockaddr_in);
+//receivefrom
+	printf("%p \n",message);
+	recvfrom (sock, message, 200, 0, (struct sockaddr*)&server, &address_length );
+
+	printf("%s\n", message);
+	return 0;
 }
